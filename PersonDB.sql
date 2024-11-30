b7caecd6beb36d916dc005faa7ae4718107a61be
Gaurav-Swami <gauravswami0404@gmail.com>
2024-12-01 00:06:59 +0530

    final bye

--- PersonDB.sql
+++ PersonDB.sql
@@ -4,7 +4,7 @@ CREATE TABLE  person(
 	city VARCHAR(100)
 );
 
-SELECT * FROM person;
+
 
 INSERT INTO person VALUES (101, 'Raju', 'Delhi');
 
@@ -14,10 +14,35 @@ INSERT INTO person VALUES (104, 'Alex', 'Pune');
 
 UPDATE person SET city = 'London' WHERE id = 102;
 
-DELETE FROM person WHERE id = 104;
+DELETE FROM person WHERE name = 'Alex';
 
 INSERT INTO person VALUES (101, 'Alex', 'Banglore');
 INSERT INTO person(id,name) VALUES (105, 'Victa');
 
+--alter table
+-- add column
+alter table person add column age int  ;
+alter table person add column age int default 0 ;
 
+-- drop column
+alter table person drop column age;
+
+-- rename column
+alter table person rename name to fname;
+
+--rename table
+alter table person rename to persons;
+alter table persons rename to person;
+
+--change datatype
+alter table person alter column fname set data type varchar(69);
+
+--set default
+alter table person alter column fname set default 'unknown';
+
+--drop default
+alter table person alter column fname  drop default;
+
+--select universal
+SELECT * FROM person;	
 
