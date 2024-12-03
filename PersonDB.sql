b4c1b3699007808b8e3266c32999048e19e635cc
Gaurav-Swami <gauravswami0404@gmail.com>
2024-12-03 23:26:03 +0530

    check constraint done

--- PersonDB.sql
+++ PersonDB.sql
@@ -15,6 +15,7 @@ INSERT INTO person VALUES (104, 'Alex', 'Pune');
 UPDATE person SET city = 'London' WHERE id = 102;
 
 DELETE FROM person WHERE name = 'Alex';
+DELETE FROM person WHERE fname IS NULL;
 
 INSERT INTO person VALUES (101, 'Alex', 'Banglore');
 INSERT INTO person(id,name) VALUES (105, 'Victa');
@@ -25,7 +26,7 @@ alter table person add column age int  ;
 alter table person add column age int default 0 ;
 
 -- drop column
-alter table person drop column age;
+alter table person drop column mob;
 
 -- rename column
 alter table person rename name to fname;
@@ -42,7 +43,19 @@ alter table person alter column fname set default 'unknown';
 
 --drop default
 alter table person alter column fname  drop default;
+	
+
+--check contraint
+alter table person add column mob varchar(15) check (length(mob)>=10);
+insert into person (mob) values(1234567890);
+
+--drop constraint
+alter table person drop constraint person_mob_check;
+
+--add constraint with specific name
+alter table person add constraint mob_no_less_than_10 check (length(mob)>= 10);
+
+
 
 --select universal
-SELECT * FROM person;	
-
+SELECT * FROM person;
